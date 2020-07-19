93c85f0d3b91c1f155fb4db8cbf937e5e02b39b8
Nelson R Manohar Alers <nelsonr@umich.edu>
2020-07-18 20:53:10 -0400

    medium one, palindrome

---
+++ PYTHON/p0005.py
@@ -0,0 +1,20 @@
+class Solution:
+    ''' Given a string s, find the longest palindromic substring in s.
+        You may assume that the maximum length of s is 1000.
+    '''
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if len(set(s)) == 1: return s
+
+        self.nmax = 1
+        self.longest = s[0:1]
+        for i in range(n):
+            for j in range(n, i+1, -1):
+                if s[i] != s[j-1]: continue
+                if s[i:j] == s[i:j][::-1]:
+                    if len(s[i:j]) > self.nmax:
+                        self.longest = s[i:j]
+                        self.nmax = len(self.longest)
+
+        return self.longest
+
